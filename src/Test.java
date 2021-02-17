db048419ad4b21895c6ec75373d4b804aa3de760
kushevsky01 <kushevskij01@mail.ru>
2021-02-17 17:40:31 +0300

    Add rsc

---
+++ src/Test.java
@@ -0,0 +1,23 @@
+public class Test {
+    public static void main(String[] args) {
+        byte bt = 1;
+        System.out.println(bt);
+        short st = 1111;
+        System.out.println(st);
+        char ct;
+        ct = 'D';
+        System.out.println(ct);
+        int it = 2001;
+        System.out.println(it);
+        long lt = 1013901;
+        System.out.println(lt);
+        boolean bot = true;
+        System.out.println(bot);
+        double dt = 1.1;
+        System.out.println(dt);
+        float ft = 1.23f;
+        System.out.println(ft);
+        String str = "Hello world";
+        System.out.println(str);
+    }
+}
